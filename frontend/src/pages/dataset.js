1495d43338efbacc2fe335f7263ea0bc265fd961
Jeffrey <43458607+marchatnite@users.noreply.github.com>
2022-04-10 22:33:30 -0500

    Completed DataSets BackEnd and Front end Runs successfully Locally
    Completed DataSets BackEnd and Front end Runs successfully Locally. Made changes to package.json dependencies.

---
+++ frontend/src/pages/dataset.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DatasetTable from '../components/Datasets'
+const DatasetPage = () => {
+    return (
+      <>
+        <>Hello</>
+        <DatasetTable></DatasetTable>
+      </>
+    );
+  }
+
+  export default DatasetPage
