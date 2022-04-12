99a71d4e070158165d6c526edc750da5f3311ecf
Jeffrey <43458607+marchatnite@users.noreply.github.com>
2022-04-12 00:53:48 -0500

    Authentication and session cookies working locally

--- frontend/src/components/Signin/index.js
+++ frontend/src/components/Signin/index.js
@@ -10,6 +10,7 @@
     SignInButton,
     Text
 } from './styles'
+import { useState } from 'react';
 /* import React, { useRef, useState } from 'react'
 import { Form, Button, Card, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
@@ -18,16 +19,77 @@ import { Container } from 'react-bootstrap'
 
  */
 
+function SignInUser(credentials){
+    const body = JSON.stringify(credentials);
+    return fetch("http://localhost:5000/signin", {
+        method: 'Post',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin':'*',
+            'Access-Control-Allow-Credentials': 'true',
+            
+        },
+        credentials: 'include',
+        body: JSON.stringify(credentials),
+    }).then(data => data.json())
+    
+}
+
+function Test(){
+    const a = fetch("http://localhost:5000/test", {
+        method: 'Post',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin':'*',
+            'Access-Control-Allow-Credentials': 'true',
+
+            
+        },
+        credentials: 'include'
+    })
+    console.log(a);
+
+}
+
 const SignIn = () => {
+
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [responseText, setResponseText] = useState();
+    
+    async function handleSubmit(){
+        console.log("You are submitting");
+
+        const credentials = {
+            email,
+            password
+        }
+        const fetchResponse = await SignInUser(credentials);
+        if(fetchResponse.hasOwnProperty('message')){
+            setResponseText(fetchResponse.message);
+        }
+        else if(fetchResponse.hasOwnProperty('error')){
+            setResponseText(fetchResponse.error);
+        }else if(fetchResponse.hasOwnProperty('email')){
+            setResponseText(fetchResponse.email);
+        }
+
+        await Test();
+
+        console.log(fetchResponse);
+
+
+    }
+
+    
+
 
 
 /* 
-    const emailRef = useRef()
-    const passwordRef = useRef()
-    const { login } = useAuth()
-    const [error, setError] = useState('')
-    const [loading, setLoading] = useState('')
-    const history = useHistory()
+
+    
+    
+    
 
     // click action for the login button
     async function handleSubmit(e) {
@@ -53,11 +115,12 @@ const SignIn = () => {
                         <SignInForm action="#">
                             <SignInH1>Sign In</SignInH1>
                             <SignInLabel>Email</SignInLabel>
-                            <SignInInput type="email" required />
+                            <SignInInput type="email" onChange={e => {setEmail(e.target.value); console.log(email)}} required />
                             <SignInLabel>Password</SignInLabel>
-                            <SignInInput type="password" required />
-                            <SignInButton type="submit">Sign In</SignInButton>
+                            <SignInInput type="password" onChange={e => setPassword(e.target.value)}required />
+                            <SignInButton type="button" onClick={() => {handleSubmit()}}>Sign In</SignInButton>
                             <Text>Need an account? <a href ="/signup">Sign up</a></Text>
+                            <Text>{responseText}</Text>
                         </SignInForm>
                     </SignInContent>
                 </SignInWrap>
